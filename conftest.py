b004039c9eef4c149764894e8b53a9ebc32a9ecf
Oksana Matviyiv <matviyiv.o@gmail.com>
2025-12-18 11:44:28 +0200

    Refactor structure: move conftest.py to root and update GitHub client and API tests with new search logic

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+from modules.api.clients.github import GitHub
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = 'Sergii'
+        self.second_name = 'Butenko'
+
+    def remove(self):
+        self.name = ''
+        self.second_name = ''
+
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+
+    user.remove()
+
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
